325afcccb91a26d86357afdced801b50d465d407
art241111 <art241111@mail.ru>
2021-04-10 12:33:47 +0300

    Adding navigation to MainActivity.kt and transferring all logic to navigation

--- app/src/main/java/com/art241111/kprizes/MainActivity.kt
+++ app/src/main/java/com/art241111/kprizes/MainActivity.kt
@@ -4,29 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.art241111.kprizes.ui.Background
-import com.art241111.kprizes.ui.startScreen.StartScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.art241111.kprizes.navigation.MainNavigateScreen
+import com.art241111.kprizes.navigation.MainNavigationVM
 import com.art241111.kprizes.ui.theme.KPrizesTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navigationVM: MainNavigationVM
+
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            navigationVM = viewModel()
+            navigationVM.moveToHome()
+
             KPrizesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Background(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        StartScreen()
-                    }
+                    MainNavigateScreen(
+                        navigate = navigationVM
+                    )
                 }
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (navigationVM.onBackButtonClick()) {
+            super.onBackPressed()
         }
     }
 }
